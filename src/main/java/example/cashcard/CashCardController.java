baddba46d31b9892cf7e4917aed1f85d3a410955
Bryan | JJ <with.beth@gmail.com>
2023-03-16 22:27:46 +0900

    feat(CashCardController): make IT pass by making the Controller return specific Cash Card only if we submit the certain id

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
     @GetMapping("/{cardId}")
-    ResponseEntity<CashCard> findById(@PathVariable String cardId) {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    ResponseEntity<CashCard> findById(@PathVariable Long cardId) {
+        // return the specific Cash Card only if we submit the correct identifier.
+        if (99L == cardId) {
+            return ResponseEntity.ok(new CashCard(cardId, 123.45));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
