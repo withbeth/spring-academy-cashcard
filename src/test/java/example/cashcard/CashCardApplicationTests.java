62a2b6e16ff3918543676dcc53842a6e3e9a69ec
Bryan | JJ <with.beth@gmail.com>
2023-03-19 02:36:30 +0900

    test: fix IT case for POST endpoint after creating dummy endpoint

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -52,10 +54,21 @@ class CashCardApplicationTests {
     @Test
     void shouldCreateANewCashCard() {
         CashCard newCashCard = new CashCard(250.00);
-        ResponseEntity<Void> response = restTemplate.postForEntity(BASE_ENDPOINT, newCashCard, Void.class);
-        // TODO : we should assert 201 CREATED
-        // TODO : we should check location header
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity(BASE_ENDPOINT, newCashCard, Void.class);
+        URI locationOfNewCard = createResponse.getHeaders().getLocation();
+
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(locationOfNewCard).isNotNull();
+
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCard, String.class);
+
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext parsedBody = JsonPath.parse(getResponse.getBody());
+        Long id = parsedBody.read("$.id", Long.class);
+        assertThat(id).isNotNull();
+
+        Double amount = parsedBody.read("$.amount", Double.class);
+        assertThat(amount).isEqualTo(250.00);
     }
 
 }
